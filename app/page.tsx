3536708cc64e23deb9d54030fda9663431e19bd0
howooking <junsgk@gmail.com>
2024-05-27 16:33:53 +0900

    library dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world
-    </main>
-  )
+  return <main>hello world</main>
 }
