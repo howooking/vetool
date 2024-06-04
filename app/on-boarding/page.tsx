3989f5ce5cfd9142e1860cbc7661780a5a88bf17
Kled <1017yu@daum.net>
2024-06-04 11:45:26 +0900

    fix: 오타 수정

--- app/on-boarding/page.tsx
+++ app/on-boarding/page.tsx
@@ -2,12 +2,14 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default async function SelectHospitalPage() {
-  ;<div className="flex">
-    <Button>
-      <Link href="/on-boarding/select-hospital">등록된 병원에 가입하기</Link>
-    </Button>
-    <Button>
-      <Link href="/on-boarding/create-hospital">새로운 병원을 등록하기</Link>
-    </Button>
-  </div>
+  return (
+    <div className="flex">
+      <Button>
+        <Link href="/on-boarding/select-hospital">등록된 병원에 가입하기</Link>
+      </Button>
+      <Button>
+        <Link href="/on-boarding/create-hospital">새로운 병원을 등록하기</Link>
+      </Button>
+    </div>
+  )
 }
