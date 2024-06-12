86dd35d91ca0f2f039e9f547cf06aa9b42d4ea84
Kled <1017yu@daum.net>
2024-06-12 14:56:42 +0900

    WIP: header 리디자인

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { Button } from '../ui/button'
 import { createClient } from '@/lib/supabase/server'
 import { logout } from '@/lib/actions/auth'
+import { Cat } from 'lucide-react'
 
 export default async function Header() {
   const supabase = createClient()
@@ -11,26 +12,31 @@ export default async function Header() {
   } = await supabase.auth.getUser()
 
   return (
-    <header className="fixed flex bg-zinc-600">
-      <Button asChild variant="default">
-        <Link href="/">Home</Link>
-      </Button>
+    <header className="sticky top-0 flex items-center justify-between border-b bg-white">
+      {/* 벳툴 로고 */}
+      <div className="flex items-center justify-center px-2 py-2">
+        <Cat size={32} color="#fff" fill="#14B8A6" />
+      </div>
 
-      <Button asChild variant="outline">
-        <Link href="/private">private</Link>
-      </Button>
+      <div className="flex">
+        <Button asChild variant="default">
+          <Link href="/">Home</Link>
+        </Button>
 
-      {user ? (
-        <>
+        <Button asChild variant="outline">
+          <Link href="/private">private</Link>
+        </Button>
+
+        {user ? (
           <form action={logout}>
             <Button variant="destructive">Logout</Button>
           </form>
-        </>
-      ) : (
-        <Button asChild variant="secondary">
-          <Link href="/login">Login</Link>
-        </Button>
-      )}
+        ) : (
+          <Button asChild variant="secondary">
+            <Link href="/login">Login</Link>
+          </Button>
+        )}
+      </div>
     </header>
   )
 }
