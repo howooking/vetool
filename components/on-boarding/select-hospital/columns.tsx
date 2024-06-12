d9be3657658d544154c0c3a021a4088405318eb7
Kled <1017yu@daum.net>
2024-06-12 14:58:04 +0900

    feat: 병원 내 환자 조회 table column 선언

---
+++ components/on-boarding/select-hospital/columns.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { ColumnDef } from '@tanstack/react-table'
+import { Checkbox } from '@/components/ui/checkbox'
+import { useSelectHospitalStore } from '@/lib/store/on-boarding/select-hospital'
+
+export type SelectHosptialColumnsType = {
+  hos_id: string
+  name: string | null
+  city: string | null
+  district: string | null
+}
+
+export const SelectHospitalColumns: ColumnDef<SelectHosptialColumnsType>[] = [
+  {
+    id: 'select',
+    cell: ({ row, table }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => {
+          // 다중 선택 방지
+          table.toggleAllPageRowsSelected(false)
+          row.toggleSelected(!!value)
+          useSelectHospitalStore.setState({
+            hosId: row.original.hos_id,
+          })
+        }}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'name',
+    header: '병원명',
+  },
+
+  {
+    accessorKey: 'city',
+    header: '지역',
+  },
+  {
+    accessorKey: 'district',
+    header: '시·군·구',
+  },
+]
