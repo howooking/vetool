38530efda3f100e3f67c2ca4e36d93ec1bcfbd89
Kled <1017yu@daum.net>
2024-06-17 14:57:55 +0900

    feat: 환자 목록 테이블 체크박스 추가

--- components/hospital/patients/columns.tsx
+++ components/hospital/patients/columns.tsx
@@ -1,55 +1,72 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { Database } from '@/lib/supabase/database.types'
+import { Checkbox } from '@/components/ui/checkbox'
+import { useSelectedPatientStore } from '@/lib/store/hospital/patients/selected-patient'
+import { Patients } from '@/types/hospital'
 import { ColumnDef } from '@tanstack/react-table'
 import { ArrowUpDown } from 'lucide-react'
 
-type HospitalPatientsColumnsType =
-  Database['public']['Tables']['patients']['Row']
-
-export const HospitalPatientsColumns: ColumnDef<HospitalPatientsColumnsType>[] =
-  [
-    {
-      accessorKey: 'name',
-      header: '이름',
+export const HospitalPatientsColumns: ColumnDef<Patients>[] = [
+  {
+    id: 'select',
+    cell: ({ row, table }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => {
+          // 다중 선택 방지
+          table.toggleAllPageRowsSelected(false)
+          row.toggleSelected(!!value)
+          useSelectedPatientStore.setState({
+            patientId: value ? row.original.patient_id : null,
+          })
+        }}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'name',
+    header: '이름',
+  },
+  {
+    accessorKey: 'microchip_no',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          마이크로칩 번호
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
     },
-    {
-      accessorKey: 'microchip_no',
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-          >
-            마이크로칩 번호
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-    },
-    {
-      accessorKey: 'species',
-      header: '종',
-    },
-    {
-      accessorKey: 'breed',
-      header: '품종',
-    },
-    {
-      accessorKey: 'gender',
-      header: '성별',
-    },
-    {
-      accessorKey: 'birth',
-      header: '생년월일',
-    },
-    {
-      accessorKey: 'status',
-      header: '상태',
-    },
-    {
-      accessorKey: 'memo',
-      header: '메모',
-    },
-  ]
+  },
+  {
+    accessorKey: 'species',
+    header: '종',
+  },
+  {
+    accessorKey: 'breed',
+    header: '품종',
+  },
+  {
+    accessorKey: 'gender',
+    header: '성별',
+  },
+  {
+    accessorKey: 'birth',
+    header: '생년월일',
+  },
+  {
+    accessorKey: 'status',
+    header: '상태',
+  },
+  {
+    accessorKey: 'memo',
+    header: '메모',
+  },
+]
