5be2c6e766c53d88b6110a89a4f85f8fe81e6460
Kled <1017yu@daum.net>
2024-06-17 14:56:35 +0900

    feat: Icu 헤더 생성

--- components/hospital/header/hospital-header.tsx
+++ components/hospital/header/hospital-header.tsx
@@ -5,31 +5,35 @@ import { Suspense } from 'react'
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
 import { UserInfoSkeleton } from './user-info-skeleton'
+import IcuHeader from '../icu/header/icu-header'
 
 export default async function HospitalHeader({ hosId }: { hosId: string }) {
   return (
-    <header className="flex h-12 items-center justify-end gap-4 border-b px-4">
-      <Suspense fallback={<UserInfoSkeleton />}>
-        <UserInfo hosId={hosId} />
-      </Suspense>
+    <header className="flex h-12 items-center justify-between gap-4 border-b px-4">
+      <IcuHeader />
 
-      <ul className="flex gap-2">
-        <li>
-          <Button size="icon" variant="secondary">
-            <Search size={16} />
-          </Button>
-        </li>
-        <li>
-          <Button size="icon" variant="secondary">
-            <Bell size={16} />
-          </Button>
-        </li>
-        <li>
-          <Button size="icon" variant="secondary">
-            <Settings size={16} />
-          </Button>
-        </li>
-      </ul>
+      <div className="flex items-center gap-4">
+        <Suspense fallback={<UserInfoSkeleton />}>
+          <UserInfo hosId={hosId} />
+        </Suspense>
+        <ul className="flex gap-2">
+          <li>
+            <Button size="icon" variant="secondary">
+              <Search size={16} />
+            </Button>
+          </li>
+          <li>
+            <Button size="icon" variant="secondary">
+              <Bell size={16} />
+            </Button>
+          </li>
+          <li>
+            <Button size="icon" variant="secondary">
+              <Settings size={16} />
+            </Button>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
