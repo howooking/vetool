f2a3a94ab64d1e9ef11241e33d357fc22ff45408
Kled <1017yu@daum.net>
2024-06-19 10:28:06 +0900

    feat: type 정의

--- types/hospital/index.ts
+++ types/hospital/index.ts
@@ -1,3 +1,4 @@
 import { Database } from '@/lib/supabase/database.types'
 
 export type Patients = Database['public']['Tables']['patients']['Row']
+export type Users = Database['public']['Tables']['users']['Row']
