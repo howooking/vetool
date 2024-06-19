85166953c50dc15d0bae7966dd563f7fd1762f15
Jungwoo Lee <junsgk@gmail.com>
2024-06-19 11:47:34 +0900

    chore: database type, auth user type definition

--- types/hospital/index.ts
+++ types/hospital/index.ts
@@ -1,3 +1,53 @@
 import { Database } from '@/lib/supabase/database.types'
 
 export type Patients = Database['public']['Tables']['patients']['Row']
+
+// supabase.auth.getUser() 시 return되는 유져타입
+export type User = {
+  id: string
+  app_metadata: {
+    provider?: string
+    [key: string]: any
+  }
+  user_metadata: {
+    [key: string]: any
+  }
+  aud: string
+  confirmation_sent_at?: string
+  recovery_sent_at?: string
+  email_change_sent_at?: string
+  new_email?: string
+  new_phone?: string
+  invited_at?: string
+  action_link?: string
+  email?: string
+  phone?: string
+  created_at: string
+  confirmed_at?: string
+  email_confirmed_at?: string
+  phone_confirmed_at?: string
+  last_sign_in_at?: string
+  role?: string
+  updated_at?: string
+  identities?: {
+    id: string
+    user_id: string
+    identity_data?: {
+      [key: string]: any
+    }
+    identity_id: string
+    provider: string
+    created_at?: string
+    last_sign_in_at?: string
+    updated_at?: string
+  }[]
+  is_anonymous?: boolean
+  factors?: {
+    id: string
+    friendly_name?: string
+    factor_type: 'totp' | string
+    status: 'verified' | 'unverified'
+    created_at: string
+    updated_at: string
+  }[]
+}
