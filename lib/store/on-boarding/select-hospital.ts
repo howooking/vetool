ad1665219a64176f1657d13abed5e29f697d7cb4
Jungwoo Lee <junsgk@gmail.com>
2024-06-23 11:48:35 +0900

    refactor: 병원 선택 전역생태 set 함수 지정

--- lib/store/on-boarding/select-hospital.ts
+++ lib/store/on-boarding/select-hospital.ts
@@ -2,8 +2,10 @@ import { create } from 'zustand'
 
 type SelectHospitalState = {
   hosId: string
+  setHosId: (hosId: string) => void
 }
 
-export const useSelectHospitalStore = create<SelectHospitalState>(() => ({
+export const useSelectHospitalStore = create<SelectHospitalState>((set) => ({
   hosId: '',
+  setHosId: () => set((state) => ({ hosId: state.hosId })),
 }))
