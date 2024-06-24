12a9103bcfa4d4f9f8e2b30f7957505ce38e36af
Kled <1017yu@daum.net>
2024-06-24 09:44:25 +0900

    refactor: icu date picker 변경

--- components/hospital/icu/header/icu-header-date-selector.tsx
+++ components/hospital/icu/header/icu-header-date-selector.tsx
@@ -2,20 +2,18 @@
 
 import IcuHeaderDatePicker from '@/components/hospital/icu/header/icu-header-date-picker'
 import { Button } from '@/components/ui/button'
-import { useSelectedDateStore } from '@/lib/store/hospital/icu/selected-date'
+import { useIcuSelectedDateStore } from '@/lib/store/hospital/icu/selected-date'
 import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
 import { format } from 'date-fns'
 
 export default function IcuHeaderDateSelector() {
-  const { selectedDate } = useSelectedDateStore()
+  const { selectedDate, setSelectedDate } = useIcuSelectedDateStore()
 
   const updateDate = (days: number) => {
     const newDate = new Date(selectedDate)
 
     newDate.setDate(newDate.getDate() + days)
-    useSelectedDateStore.setState({
-      selectedDate: format(newDate, 'yyyy-MM-dd'),
-    })
+    setSelectedDate(format(newDate, 'yyyy-MM-dd'))
   }
 
   return (
@@ -31,7 +29,10 @@ export default function IcuHeaderDateSelector() {
 
       <div className="flex items-center gap-1">
         <span className="min-w-20 text-sm">{selectedDate}</span>
-        <IcuHeaderDatePicker />
+        <IcuHeaderDatePicker
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+        />
       </div>
 
       <Button
