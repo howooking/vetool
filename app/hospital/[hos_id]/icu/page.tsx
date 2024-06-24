4178df4d2170eb4cd0b2c1dd2dc95afe7de8e5e6
Kled <1017yu@daum.net>
2024-06-24 17:41:20 +0900

    refactor: [chart_id] route 삭제

--- app/hospital/[hos_id]/icu/page.tsx
+++ app/hospital/[hos_id]/icu/page.tsx
@@ -1,5 +1,7 @@
+import IcuChart from '@/components/hospital/icu/chart/icu-chart'
 import IcuDialog from '@/components/hospital/icu/dialog/icu-dialog'
 import { createClient } from '@/lib/supabase/server'
+import type { IcuChartJoined } from '@/types/hospital'
 
 export default async function IcuPage({
   params,
@@ -8,13 +10,33 @@ export default async function IcuPage({
 }) {
   const hosId = params.hos_id
   const supabase = createClient()
+
+  const { data: icuChartData, error: icuChartError } = await supabase
+    .from('icu_chart')
+    .select(
+      `*,
+        icu_io_id(*),
+        patient_id("name", "gender", "breed", "patient_id"),
+        main_vet("name", "user_id"),
+        sub_vet("name", "user_id")
+        `,
+    )
+    .match({ hos_id: params.hos_id })
+    .order('created_at', { ascending: true })
+    .returns<IcuChartJoined[]>()
+
+  if (icuChartError) {
+    console.log(icuChartError)
+    throw new Error(icuChartError.message)
+  }
+
   const { data: patientsData, error: patientsError } = await supabase
     .from('patients')
     .select('*')
     .match({ hos_id: hosId })
 
   if (patientsError) {
-    console.log(patientsError.message)
+    console.log(patientsError)
     throw new Error(patientsError.message)
   }
 
@@ -24,7 +46,7 @@ export default async function IcuPage({
     .match({ hos_id: hosId })
 
   if (vetsError) {
-    console.log(vetsError.message)
+    console.log(vetsError)
     throw new Error(vetsError.message)
   }
 
@@ -34,13 +56,13 @@ export default async function IcuPage({
     .match({ hos_id: hosId })
 
   if (groupListError) {
-    console.log(groupListError.message)
+    console.log(groupListError)
     throw new Error(groupListError.message)
   }
 
   return (
-    <div>
-      <span>IcuPage</span>
+    <div className="w-full">
+      <IcuChart icuChartData={icuChartData} vetsData={vetsData} />
       <IcuDialog
         hosId={hosId}
         patients={patientsData}
