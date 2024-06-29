d978317b60a9efc64b2833d09be64340225effc0
Jungwoo Lee <junsgk@gmail.com>
2024-06-29 22:45:13 +0900

    refactor: remove header-title component

--- components/hospital/header/hospital-header.tsx
+++ components/hospital/header/hospital-header.tsx
@@ -9,14 +9,7 @@ import PatientsHeader from '../patients/patients-header'
 
 export default async function HospitalHeader({ hosId }: { hosId: string }) {
   return (
-    <header className="flex h-12 items-center justify-between gap-4 border-b px-2">
-      {/* 굳이 있어야 하나 고민중 */}
-      {/* <HeaderTitle /> */}
-      <div>
-        <IcuHeader />
-        <PatientsHeader hosId={hosId} />
-      </div>
-
+    <header className="flex h-12 items-center justify-end gap-4 border-b px-2">
       <div className="flex items-center gap-4">
         <Suspense fallback={<UserInfoSkeleton />}>
           <UserInfo hosId={hosId} />
