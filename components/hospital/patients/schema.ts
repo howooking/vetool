ac96d53dbaaf1c58bb1d05dd6b517406ff3abbc9
Jungwoo Lee <junsgk@gmail.com>
2024-06-30 00:47:23 +0900

    feat: 보호자 폼

--- components/hospital/patients/schema.ts
+++ components/hospital/patients/schema.ts
@@ -11,3 +11,19 @@ export const registerPatientFormSchema = z.object({
   microchip_no: z.string({ required_error: '마이크로칩 넘버를 입력해주세요.' }),
   memo: z.string({ required_error: '종을 선택해주세요.' }),
 })
+
+export const ownerFormSchema = z.object({
+  owner_name: z
+    .string({ required_error: '보호자 이름을 입력해주세요.' })
+    .trim()
+    .min(1, { message: '보호자 이름을 입력해주세요.' }),
+  owner_address: z.string().default(''),
+  owner_phone_number: z.string().default(''),
+  owner_memo: z.string().default(''),
+  hos_owner_id: z
+    .string({
+      required_error: '보호자 번호를 입력해주세요.',
+    })
+    .trim()
+    .min(1, { message: '보호자 번호를 입력해주세요.' }),
+})
