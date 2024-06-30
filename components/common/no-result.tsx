db7403ab28fda5f0bad79d2fc49407a2058a0f7c
Jungwoo Lee <junsgk@gmail.com>
2024-06-30 21:31:42 +0900

    refactor: 온보딩 과정 중 생성된 병원이 없는 경우 UI 핸들링

--- components/common/no-result.tsx
+++ components/common/no-result.tsx
@@ -1,10 +1,24 @@
+import { cn } from '@/lib/utils'
 import { SearchX } from 'lucide-react'
 
-export default function NoResult({ title }: { title: string }) {
+export default function NoResult({
+  title,
+  className,
+  ...rest
+}: {
+  title: string
+  className?: string
+}) {
   return (
-    <div className="hospital-page-height flex w-full flex-col items-center justify-center gap-4 pb-20">
-      <SearchX className="text-primary" size={96} />
-      <span className="text-3xl font-bold">{title}</span>
+    <div
+      className={cn(
+        'hospital-page-height flex w-full flex-col items-center justify-center gap-4 pb-20',
+        className,
+      )}
+      {...rest}
+    >
+      <SearchX className="text-primary" size={80} />
+      <span className="text-2xl font-bold">{title}</span>
     </div>
   )
 }
