ca2b3c84fdca959b345998f1b4ff72d4015c683e
Kled <1017yu@daum.net>
2024-07-05 13:32:43 +0900

    faet: chartTable에 vetsData 추가

--- components/hospital/icu/main/chart/icu-chart-order.tsx
+++ components/hospital/icu/main/chart/icu-chart-order.tsx
@@ -1,7 +1,7 @@
 import type { IcuChartJoined, IcuChartOrderJoined, Vet } from '@/types/hospital'
 import IcuChartPatientDetail from './patient-detail/icu-chart-patient-detail'
 import IcuChartTable from './table/icu-chart-table'
-import IcuChartMemos from './memos/icu-chart-memos'
+import IcuChartMemo from './memo/icu-chart-memo'
 
 export default function IcuChartOrder({
   selectedChart,
@@ -16,8 +16,11 @@ export default function IcuChartOrder({
   return (
     <div className="flex flex-col gap-2 pb-[48px]">
       <IcuChartPatientDetail chartData={restChartData} vetsData={vetsData} />
-      <IcuChartTable selectedChartOrders={selectedChartOrders} />
-      <IcuChartMemos memoA={memo_a} memoB={memo_b} memoC={memo_c} />
+      <IcuChartTable
+        selectedChartOrders={selectedChartOrders}
+        vetsData={vetsData}
+      />
+      <IcuChartMemo memoA={memo_a} memoB={memo_b} memoC={memo_c} />
     </div>
   )
 }
