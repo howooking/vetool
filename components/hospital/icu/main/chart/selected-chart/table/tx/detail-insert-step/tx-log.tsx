6192d883f9d24c49df7d766c231bea3f71602577
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-07-19 14:03:54 +0900

    refactor: div in table warning 해결

--- components/hospital/icu/main/chart/selected-chart/table/tx/detail-insert-step/tx-log.tsx
+++ components/hospital/icu/main/chart/selected-chart/table/tx/detail-insert-step/tx-log.tsx
@@ -5,14 +5,7 @@ import {
   AccordionTrigger,
 } from '@/components/ui/accordion'
 import { Label } from '@/components/ui/label'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import type { TxLog } from '@/types/icu'
 
 export default function TxLog({ logs }: { logs: TxLog[] }) {
@@ -21,24 +14,16 @@ export default function TxLog({ logs }: { logs: TxLog[] }) {
       <Label>처치 기록</Label>
       <Accordion type="single" collapsible className="mt-2">
         <AccordionItem value="logs">
-          <Table>
-            <AccordionTrigger className="py-0">
-              <TableHeader className="w-full">
-                <TableRow className="flex w-full items-center py-2">
-                  <TableHead className="h-auto w-2/5 text-center">
-                    결과
-                  </TableHead>
-                  <TableHead className="h-auto w-1/5 pl-6 text-center">
-                    담당자
-                  </TableHead>
-                  <TableHead className="h-auto w-2/5 pr-8 text-right">
-                    시간
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-            </AccordionTrigger>
-            <AccordionContent className="p-0">
-              <TableBody className="flex flex-col">
+          <AccordionTrigger className="py-1">
+            <div className="flex w-full items-center py-2">
+              <div className="w-2/5 text-center">결과</div>
+              <div className="w-1/5 pl-6 text-center">담당자</div>
+              <div className="w-2/5 pr-8 text-right">시간</div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="pb-0">
+            <Table>
+              <TableBody>
                 {logs.map((log, index) => (
                   <TableRow
                     className="flex w-full items-center justify-between"
@@ -59,8 +44,8 @@ export default function TxLog({ logs }: { logs: TxLog[] }) {
                   </TableRow>
                 ))}
               </TableBody>
-            </AccordionContent>
-          </Table>
+            </Table>
+          </AccordionContent>
         </AccordionItem>
       </Accordion>
     </div>
