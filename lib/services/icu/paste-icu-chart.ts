ecf604045e9a67ed739014b5c9e802581d83f1c5
Kled Yu <83483378+kledyu@users.noreply.github.com>
2024-07-19 14:41:11 +0900

    Create paste-icu-chart.ts

---
+++ lib/services/icu/paste-icu-chart.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+import type { IcuChartOrderJoined } from '@/types/icu'
+import { redirect } from 'next/navigation'
+
+export const pasteIcuChart = async (
+  targetDate: string,
+  patientId: string,
+  selectedIcuChartId: string,
+  copiedChartOrder: IcuChartOrderJoined[],
+) => {
+  const supabase = createClient()
+
+  const { data: returningIcuChartIds, error: rpcError } = await supabase.rpc(
+    'paste_icu_chart_after_search_chart',
+    {
+      target_date_input: targetDate,
+      patient_id_input: patientId,
+      icu_chart_id_input: selectedIcuChartId,
+    },
+  )
+
+  if (rpcError) {
+    console.log(rpcError)
+    redirect(`/error?message=${rpcError.message}`)
+  }
+
+  const { newIcuChartId, icuIoId } = returningIcuChartIds as {
+    newIcuChartId: string
+    icuIoId: string
+  }
+
+  copiedChartOrder.forEach(async (order) => {
+    const { error: icuChartOrderError } = await supabase
+      .from('icu_chart_order')
+      .insert({
+        hos_id: order.hos_id,
+        icu_chart_order_type: order.icu_chart_order_type,
+        icu_chart_id: newIcuChartId,
+        icu_io_id: icuIoId,
+        icu_chart_order_name: order.icu_chart_order_name,
+        icu_chart_order_comment: order.icu_chart_order_comment,
+        icu_chart_order_time: order.icu_chart_order_time,
+      })
+
+    if (icuChartOrderError) {
+      console.log(icuChartOrderError)
+      redirect(`/error?message=${icuChartOrderError.message}`)
+    }
+  })
+}
