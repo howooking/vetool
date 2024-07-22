bbe8bc10485f949576d9cdb7e75f4421c21adbd3
Kled <1017yu@daum.net>
2024-07-22 11:41:14 +0900

    style: setter name 변경

--- lib/store/icu/order-preview.ts
+++ lib/store/icu/order-preview.ts
@@ -2,10 +2,10 @@ import { create } from 'zustand'
 
 type OrderPreviewState = {
   isPreviewModalOpen: boolean
-  onOpenChange: (isOpen: boolean) => void
+  setPreviewModalOpen: (isOpen: boolean) => void
 }
 
 export const useOrderPreviewStore = create<OrderPreviewState>((set) => ({
   isPreviewModalOpen: false,
-  onOpenChange: (state) => set({ isPreviewModalOpen: state }),
+  setPreviewModalOpen: (state) => set({ isPreviewModalOpen: state }),
 }))
