fe09fdeffa9f1f990127a946cac8c907b43411cb
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-07-23 08:30:24 +0900

    feat: 처치표 기능 구현, 스타일링 레이아웃 미완성

--- lib/services/icu/bookmark.ts
+++ lib/services/icu/bookmark.ts
@@ -55,7 +55,5 @@ export const getBookmarkChart = async () => {
     redirect(`/error/?message=${selectedBookmarkChartError.message}`)
   }
 
-  console.log(selectedBookmarkChart)
-
   return selectedBookmarkChart
 }
