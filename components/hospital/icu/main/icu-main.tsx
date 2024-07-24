0eefa9180351142f4484af500980e391da322599
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-07-24 14:58:04 +0900

    refactor: 환자가 없을 경우 종합현황 및 처치표에서 안내문구 뜨도록

--- components/hospital/icu/main/icu-main.tsx
+++ components/hospital/icu/main/icu-main.tsx
@@ -2,9 +2,9 @@
 
 import IcuChart from '@/components/hospital/icu/main/chart/icu-chart'
 import IcuChartSearch from '@/components/hospital/icu/main/search/icu-search-chart'
-import IcuSummary from '@/components/hospital/icu/main/summary/icu-summary'
 import { useSelectedMainViewStore } from '@/lib/store/icu/selected-main-view'
 import type { IcuData } from '@/types/icu'
+import Summary from './summary/summary'
 import Todo from './todo/todo'
 
 export default function IcuMain({ icuData }: { icuData: IcuData }) {
@@ -12,7 +12,7 @@ export default function IcuMain({ icuData }: { icuData: IcuData }) {
 
   return (
     <>
-      {selectIcudMainView === 'summary' && <IcuSummary icuData={icuData} />}
+      {selectIcudMainView === 'summary' && <Summary icuData={icuData} />}
 
       {selectIcudMainView === 'todo' && <Todo icuData={icuData} />}
 
