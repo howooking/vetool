8d722ea3a259a616a62c49fc9b7d2982f0733358
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-07-26 18:32:45 +0900

    refactor: 북마크 form trim

--- components/hospital/icu/main/chart/selected-chart/chart-infos/infos/header-signament/schema.ts
+++ components/hospital/icu/main/chart/selected-chart/chart-infos/infos/header-signament/schema.ts
@@ -13,6 +13,7 @@ export const weightFormSchema = z.object({
 export const bookmarkFormSchema = z.object({
   bookmark_name: z
     .string({ required_error: '즐겨찾기 이름을 입력해주세요' })
+    .trim()
     .min(1, {
       message: '즐겨찾기 이름을 입력해주세요',
     }),
