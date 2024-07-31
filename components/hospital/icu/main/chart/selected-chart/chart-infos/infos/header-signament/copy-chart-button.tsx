4c8c1c25a91c1ba4e89e10e6a68821ac09935eb1
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-07-31 20:22:25 +0900

    feat: 차트 복사 버튼 헤더

---
+++ components/hospital/icu/main/chart/selected-chart/chart-infos/infos/header-signament/copy-chart-button.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import { toast } from '@/components/ui/use-toast'
+import { getSelectedChartOrders } from '@/lib/services/icu/search-charts'
+import { useCopiedChartStore } from '@/lib/store/icu/copied-chart'
+import { Copy, CopyCheck, LoaderCircle } from 'lucide-react'
+import { useMemo, useState } from 'react'
+
+export default function CopyChartButton({ chartId }: { chartId: string }) {
+  const [isCopying, setIsCopying] = useState(false)
+  const { copiedChartId, setCopiedChartId, setCopiedChartOrders } =
+    useCopiedChartStore()
+
+  const handleCopy = async () => {
+    setIsCopying(true)
+
+    setCopiedChartId(chartId)
+
+    const selectedChartOrders = await getSelectedChartOrders(chartId)
+    setCopiedChartOrders(selectedChartOrders)
+
+    toast({
+      title: '차트 복사 완료',
+      description: '붙여넣기 할 차트로 이동해주세요',
+    })
+
+    setIsCopying(false)
+  }
+
+  const isCopied = useMemo(
+    () => copiedChartId === chartId,
+    [chartId, copiedChartId],
+  )
+
+  return (
+    <Button variant="ghost" size="icon">
+      {isCopying ? (
+        <LoaderCircle className="h-4 w-4 animate-spin" />
+      ) : isCopied ? (
+        <CopyCheck />
+      ) : (
+        <Copy onClick={handleCopy} />
+      )}
+    </Button>
+  )
+}
