437638eb58bee038bd40c82776a7f466aa82cdc9
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-08-20 09:49:14 +0900

    refactor: useLongPress 커스텀 훅

--- components/hospital/icu/main/chart/selected-chart/table/chart-table-cell.tsx
+++ components/hospital/icu/main/chart/selected-chart/table/chart-table-cell.tsx
@@ -1,12 +1,13 @@
 import { Input } from '@/components/ui/input'
 import { TableCell } from '@/components/ui/table'
 import { toast } from '@/components/ui/use-toast'
+import { useLongPress } from '@/hooks/use-long-press'
 import { useUpsertTxStore } from '@/lib/store/icu/upsert-tx'
 import { cn } from '@/lib/utils'
 import type { IcuChartTx } from '@/types'
 import type { TxLog } from '@/types/icu'
 import { LoaderCircle } from 'lucide-react'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 export default function ChartTableCell({
   time,
@@ -39,11 +40,38 @@ export default function ChartTableCell({
     }
   }, [txData?.icu_chart_tx_result, step])
 
-  const hasComment = !!txData?.icu_chart_tx_comment
+  const targetedIsUpserting = useMemo(
+    () =>
+      isTxUpserting &&
+      time === txLocalState?.time &&
+      txLocalState.icuChartOrderId === icuChartOrderId,
+    [
+      icuChartOrderId,
+      isTxUpserting,
+      time,
+      txLocalState?.icuChartOrderId,
+      txLocalState?.time,
+    ],
+  )
 
-  const longPressTimeoutRef = useRef<NodeJS.Timeout | null>(null)
+  const longPressEvents = useLongPress({
+    onLongPress: () => {
+      setTxLocalState({
+        icuChartOrderId,
+        icuIoId,
+        txResult: txData?.icu_chart_tx_result,
+        txComment: txData?.icu_chart_tx_comment,
+        txImages: txData?.icu_chart_tx_images,
+        txId: icuChartTxId,
+        time,
+        txLog: txData?.icu_chart_tx_log as TxLog[] | null,
+      })
+      setStep('detailInsert')
+    },
+    delay: 600,
+  })
 
-  const handleUpsertBriefTxResultInput = async () => {
+  const handleUpsertBriefTxResultInput = useCallback(async () => {
     if ((txData?.icu_chart_tx_result ?? '') === briefTxResultInput.trim()) {
       setBriefTxResultInput(briefTxResultInput.trim())
       return
@@ -67,56 +95,32 @@ export default function ChartTableCell({
     })
 
     setStep('seletctUser')
-  }
+  }, [
+    briefTxResultInput,
+    icuChartOrderId,
+    icuChartTxId,
+    icuIoId,
+    setStep,
+    setTxLocalState,
+    time,
+    txData?.icu_chart_tx_result,
+  ])
 
-  const handleLongClickStart = () => {
-    setTxLocalState({
-      icuChartOrderId,
-      icuIoId,
-      txResult: txData?.icu_chart_tx_result,
-      txComment: txData?.icu_chart_tx_comment,
-      txImages: txData?.icu_chart_tx_images,
-      txId: icuChartTxId,
-      time,
-      txLog: txData?.icu_chart_tx_log as TxLog[] | null,
-    })
-
-    longPressTimeoutRef.current = setTimeout(() => {
-      setBriefTxResultInput(txData?.icu_chart_tx_result ?? '')
-      setStep('detailInsert')
-    }, 500)
-  }
-
-  const handleLongClickEnd = () => {
-    if (longPressTimeoutRef.current) {
-      clearTimeout(longPressTimeoutRef.current)
-    }
-  }
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      const target = e.currentTarget
-      setTimeout(() => {
-        if (target) {
-          target.blur()
-        }
-      }, 0)
-    }
-  }
-
-  const targetedIsUpserting = useMemo(
-    () =>
-      isTxUpserting &&
-      time === txLocalState?.time &&
-      txLocalState.icuChartOrderId === icuChartOrderId,
-    [
-      icuChartOrderId,
-      isTxUpserting,
-      time,
-      txLocalState?.icuChartOrderId,
-      txLocalState?.time,
-    ],
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        const target = e.currentTarget
+        setTimeout(() => {
+          if (target) {
+            target.blur()
+          }
+        }, 0)
+      }
+    },
+    [],
   )
+
+  const hasComment = !!txData?.icu_chart_tx_comment
 
   return (
     <TableCell className="p-0">
@@ -130,19 +134,13 @@ export default function ChartTableCell({
               'rounded-none border-none border-primary px-1 text-center outline-none ring-inset ring-primary focus-visible:ring-2 focus-visible:ring-primary',
               hasOrder && 'bg-rose-100/60',
               isDone && 'bg-green-100/60',
-              hasComment && '',
             )}
             disabled={preview || isTxUpserting}
             value={briefTxResultInput}
             onChange={(e) => setBriefTxResultInput(e.target.value)}
             onBlur={handleUpsertBriefTxResultInput}
             onKeyDown={handleKeyDown}
-            onTouchStart={handleLongClickStart}
-            onTouchEnd={handleLongClickEnd}
-            onTouchCancel={handleLongClickEnd}
-            onMouseDown={handleLongClickStart}
-            onMouseUp={handleLongClickEnd}
-            onMouseLeave={handleLongClickEnd}
+            {...longPressEvents}
           />
           {hasComment && (
             <div className="absolute right-0 top-0 border-l-[10px] border-t-[10px] border-l-transparent border-t-amber-400" />
