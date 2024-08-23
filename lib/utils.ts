9677fe15f9a19ffc01d513cfc59053ce0aa4ae0c
Kled <1017yu@daum.net>
2024-08-23 10:08:10 +0900

    WIP: 매 시각 이행하지 않은 tx 알림

--- lib/utils.ts
+++ lib/utils.ts
@@ -115,18 +115,18 @@ export const getYesterdayTodayTomorrow = () => {
   const tomorrow = new Date(koreaTime)
   tomorrow.setDate(today.getDate() + 1)
 
-  const formatDate = (date: Date) => {
-    const year = date.getFullYear()
-    const month = String(date.getMonth() + 1).padStart(2, '0')
-    const day = String(date.getDate()).padStart(2, '0')
-    return `${year}-${month}-${day}`
-  }
-
   return {
     yesterday: formatDate(yesterday),
     today: formatDate(today),
     tomorrow: formatDate(tomorrow),
   }
+}
+
+export const formatDate = (date: Date) => {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
 }
 
 // stringifiedHashtagKeywords('사과, 바나나') => '#사과#바나나'
