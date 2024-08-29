4e6fc67c02d6eb051a18660faee393256b1b47af
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-08-29 11:00:22 +0900

    feat: 입원환자 차트 table header 스티키 고정

--- components/hospital/icu/notification/unread-count.tsx
+++ components/hospital/icu/notification/unread-count.tsx
@@ -1,7 +1,7 @@
 export default function UnReadCount({ unReadCount }: { unReadCount: number }) {
   return (
     // !!bg-red-500 보단 일관성있게 destructive 사용.
-    <div className="absolute -top-1 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-destructive text-[10px] text-white">
+    <div className="absolute -top-0.5 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-destructive text-[9px] text-white">
       {unReadCount >= 10 ? '10+' : unReadCount}
     </div>
   )
