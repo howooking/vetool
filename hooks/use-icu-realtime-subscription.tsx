cce0aaa2be9b340045cf0f66e1263ed9b871ef8d
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-09-08 23:31:43 +0900

    refactor: 리얼타임 fetch interval, 접속 상태 ui

--- hooks/use-icu-realtime-subscription.tsx
+++ hooks/use-icu-realtime-subscription.tsx
@@ -1,158 +1,91 @@
 import { createClient } from '@/lib/supabase/client'
 import { useRouter } from 'next/navigation'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useRef, useCallback, useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 
 const supabase = createClient()
-const tables = ['icu_io', 'icu_chart', 'icu_chart_order', 'icu_chart_tx']
+const TABLES = ['icu_io', 'icu_chart', 'icu_chart_order', 'icu_chart_tx']
 
 export function useIcuRealTimeSubscription(hosId: string) {
-  // const subscriptionsRef = useRef<Array<ReturnType<typeof supabase.channel>>>(
-  //   [],
-  // )
-  // const statusRef = useRef<'connected' | 'disconnected'>('disconnected')
-
+  const [isSubscriptionReady, setIsSubscriptionReady] = useState(false)
   const { refresh } = useRouter()
-
-  const debouncedRefresh = useDebouncedCallback(() => {
-    console.log(`%cicu_order changed`, 'background:green; color:white')
-    refresh()
-  }, 500)
+  const debouncedRefresh = useDebouncedCallback(refresh, 500)
+  const subscriptionRef = useRef<ReturnType<typeof supabase.channel> | null>(
+    null,
+  )
 
   const handleChange = useCallback(
-    (table: string) => {
-      // if (table === 'icu_chart_order') {
-      //   debouncedRefresh()
-      // } else {
+    (payload: any) => {
       console.log(
-        `%c${table} changed`,
-        `background:${table === 'icu_chart' ? 'red' : 'blue'}; color:white`,
+        `%c${payload.table.toUpperCase()} ${payload.eventType.toUpperCase()}`,
+        `background:${getColor(payload.table)}; color:white`,
       )
-      refresh()
-      // }
+      console.log('Payload:', payload)
+      debouncedRefresh()
     },
-    [refresh],
+    [debouncedRefresh],
   )
 
   useEffect(() => {
-    const subscription = supabase
-      .channel('icu_changes')
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'icu_io',
-          filter: `hos_id=eq.${hosId}`,
-        },
-        () => handleChange('icu_io'),
-      )
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'icu_chart',
-          filter: `hos_id=eq.${hosId}`,
-        },
-        () => handleChange('icu_chart'),
-      )
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'icu_chart_order',
-          filter: `hos_id=eq.${hosId}`,
-        },
-        () => handleChange('icu_chart_order'),
-      )
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'icu_chart_tx',
-          filter: `hos_id=eq.${hosId}`,
-        },
-        () => handleChange('icu_chart_tx'),
-      )
-      .subscribe()
+    const channel = supabase.channel(`icu_realtime_${hosId}`)
+
+    TABLES.forEach((table) => {
+      channel
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table,
+            filter: `hos_id=eq.${hosId}`,
+          },
+          handleChange,
+        )
+        .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+            table,
+            filter: `hos_id=eq.${hosId}`,
+          },
+          handleChange,
+        )
+        .on(
+          'postgres_changes',
+          { event: 'DELETE', schema: 'public', table },
+          handleChange,
+        )
+    })
+
+    subscriptionRef.current = channel.subscribe((status) => {
+      if (status === 'SUBSCRIBED') {
+        setIsSubscriptionReady(true)
+        console.log('Subscribed to all tables')
+      }
+    })
 
     return () => {
-      supabase.removeChannel(subscription)
+      if (subscriptionRef.current) {
+        supabase.removeChannel(subscriptionRef.current)
+      }
     }
-  }, [handleChange, hosId])
+  }, [hosId, handleChange])
 
-  // const updateStatus = useCallback(
-  //   (newStatus: 'connected' | 'disconnected') => {
-  //     if (newStatus !== statusRef.current) {
-  //       statusRef.current = newStatus
-  //       console.log('Status updated:', newStatus)
-  //     }
-  //   },
-  //   [],
-  // )
+  return isSubscriptionReady
+}
 
-  // const setupSubscriptions = useCallback(() => {
-  //   let allConnected = true
-  //   subscriptionsRef.current = tables.map((table) =>
-  //     supabase
-  //       .channel(`${table}_changes`)
-  //       .on(
-  //         'postgres_changes',
-  //         {
-  //           event: '*',
-  //           schema: 'public',
-  //           table,
-  //           filter: `hos_id=eq.${hosId}`,
-  //         },
-  //         () => handleChange(table),
-  //       )
-  //       .subscribe((status) => {
-  //         if (status === 'SUBSCRIBED') {
-  //           console.log(`${table} channel connected`)
-  //         } else {
-  //           console.log(`${table} channel status: ${status}`)
-  //           allConnected = false
-  //         }
-  //       }),
-  //   )
-  //   updateStatus(allConnected ? 'connected' : 'disconnected')
-  // }, [handleChange, hosId, updateStatus])
-
-  // const reconnect = useCallback(() => {
-  //   console.log('Attempting to reconnect...')
-  //   subscriptionsRef.current.forEach((subscription) =>
-  //     subscription.unsubscribe(),
-  //   )
-  //   setupSubscriptions()
-  // }, [setupSubscriptions])
-
-  // useEffect(() => {
-  //   setupSubscriptions()
-
-  //   // 10초마다 웹소켓 커넥션 확인
-  //   const reconnectInterval = setInterval(() => {
-  //     if (statusRef.current === 'disconnected') {
-  //       reconnect()
-  //     }
-  //   }, 10000)
-
-  //   const handleVisibilityChange = () => {
-  //     if (!document.hidden && statusRef.current === 'disconnected') {
-  //       reconnect()
-  //     }
-  //   }
-
-  //   document.addEventListener('visibilitychange', handleVisibilityChange)
-
-  //   return () => {
-  //     subscriptionsRef.current.forEach((subscription) =>
-  //       subscription.unsubscribe(),
-  //     )
-  //     // clearInterval(reconnectInterval)
-  //     // document.removeEventListener('visibilitychange', handleVisibilityChange)
-  //   }
-  // }, [reconnect, refresh, setupSubscriptions])
+function getColor(table: string): string {
+  switch (table) {
+    case 'icu_io':
+      return 'blue'
+    case 'icu_chart':
+      return 'red'
+    case 'icu_chart_order':
+      return 'green'
+    case 'icu_chart_tx':
+      return 'purple'
+    default:
+      return 'gray'
+  }
 }
