3c5d58baddf3859bd29ec951fe71722d90ef1693
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-09-09 10:30:38 +0900

    fix: 루시드 아이콘 타입 에러 수정

--- components/hospital/admin/admin-sidebar-item.tsx
+++ components/hospital/admin/admin-sidebar-item.tsx
@@ -2,10 +2,9 @@
 
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import { LucideProps } from 'lucide-react'
+import type { LucideIcon } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { ForwardRefExoticComponent, RefAttributes } from 'react'
 
 export default function AdminSidebarItem({
   name,
@@ -14,9 +13,7 @@ export default function AdminSidebarItem({
 }: {
   name: string
   path: string
-  icon: ForwardRefExoticComponent<
-    Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
-  >
+  icon: LucideIcon
 }) {
   const pathname = usePathname()
   const isActive = pathname.split('/').at(-1) === path
