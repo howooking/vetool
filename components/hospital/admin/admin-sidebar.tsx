22931dce1f6c25e89585eb9bc8772aafffc0466b
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-09-13 15:39:36 +0900

    bugfix: 배포 에러 수정

--- components/hospital/admin/admin-sidebar.tsx
+++ components/hospital/admin/admin-sidebar.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { ADMIN_SIDE_BAR_ITEMS } from '@/app/hospital/[hos_id]/admin/layout'
 import AdminSidebarItem from '@/components/hospital/admin/admin-sidebar-item'
+import { ADMIN_SIDEBAR_ITEMS } from '@/constants/admin/admin-sidebar-items'
 
 export default function AdminSidebar() {
   return (
     <aside className="hidden h-icu-chart w-48 border-r md:block">
       <ul className="flex flex-col gap-2 p-2">
-        {ADMIN_SIDE_BAR_ITEMS.map((item) => (
+        {ADMIN_SIDEBAR_ITEMS.map((item) => (
           <AdminSidebarItem
             key={item.name}
             icon={item.icon}
---
+++ constants/admin/admin-sidebar-items.ts
@@ -0,0 +1,47 @@
+import {
+  BarChart3,
+  Pill,
+  Syringe,
+  TestTubeDiagonal,
+  UserCheck,
+  Users,
+  Utensils,
+} from 'lucide-react'
+
+export const ADMIN_SIDEBAR_ITEMS = [
+  {
+    name: '스태프관리',
+    path: 'staff',
+    icon: Users,
+  },
+  {
+    name: '사용승인',
+    path: 'approval',
+    icon: UserCheck,
+  },
+  {
+    name: '입원차트 설정',
+    path: 'icu-settings',
+    icon: Syringe,
+  },
+  {
+    name: '데이터분석',
+    path: 'statistics',
+    icon: BarChart3,
+  },
+  {
+    name: '약물설정',
+    path: 'drug-settings',
+    icon: Pill,
+  },
+  {
+    name: '사료설정',
+    path: 'food-settings',
+    icon: Utensils,
+  },
+  {
+    name: '검사설정',
+    path: 'test-settings',
+    icon: TestTubeDiagonal,
+  },
+] as const
