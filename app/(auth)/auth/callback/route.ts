d42eb817bc5c5f2c75910c445d5f46ee622b1497
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-09-16 16:47:05 +0900

    chore: 삭제된 파일 복구

---
+++ app/(auth)/auth/callback/route.ts
@@ -0,0 +1,31 @@
+import { createClient } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const supabase = createClient()
+
+  const code = searchParams.get('code')
+  if (code) {
+    const { error: codeExchangeError } =
+      await supabase.auth.exchangeCodeForSession(code)
+
+    if (codeExchangeError) {
+      console.log(codeExchangeError)
+      return NextResponse.redirect(
+        new URL(`/error?message=${codeExchangeError.message}`, request.url),
+      )
+    }
+
+    const { error } = await supabase.auth.getUser()
+
+    if (error) {
+      console.log(error)
+      return NextResponse.redirect(
+        new URL(`/error?message=${error.message}`, request.url),
+      )
+    }
+
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+}
