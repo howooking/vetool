ea95fcb1d801ebc4cf0bda8ee0b96c117d8f0443
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-09-29 14:24:25 +0900

    refactor: 퇴원 환자 커버

--- components/hospital/icu/main/chart/chart.tsx
+++ components/hospital/icu/main/chart/chart.tsx
@@ -1,13 +1,16 @@
 import type { SelectedChart } from '@/types/icu'
 import ChartBody from './selected-chart/chart-body/chart-body'
 import ChartHeader from './selected-chart/chart-header/chart-header'
+import OutPatientCover from './selected-chart/out-patient-cover'
 
 export default function Chart({ chartData }: { chartData: SelectedChart }) {
+  const isPatientOut = chartData?.icu_io?.out_date !== null
   return (
     <div className="relative flex flex-col gap-2 p-2 pb-[48px]">
       <ChartHeader chartData={chartData} />
       <ChartBody chartData={chartData} />
-      {/* {isPatientOut && <OutPatientCover />} */}
+
+      {isPatientOut && <OutPatientCover />}
     </div>
   )
 }
