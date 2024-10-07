4fd8cda8f10db034ebf8b6d4eeea4fcd20bfea24
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-10-07 09:32:47 +0900

    refactor: hydration error fix

--- components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import LargeLoaderCircle from '@/components/common/large-loader-circle'
 import OrderDialog from '@/components/hospital/icu/main/chart/selected-chart/chart-body/table/order/order-dialog'
 import TxUpsertDialog from '@/components/hospital/icu/main/chart/selected-chart/chart-body/table/tx/tx-upsert-dialog'
 import {
@@ -12,8 +13,8 @@ import {
 import { DEFAULT_ICU_ORDER_TYPE } from '@/constants/hospital/icu/chart/order'
 import { TIMES } from '@/constants/hospital/icu/chart/time'
 import { useIcuOrderStore } from '@/lib/store/icu/icu-order'
-import type { SelectedChart } from '@/types/icu/chart'
-import { useMemo } from 'react'
+import type { SelectedChart, SelectedIcuOrder } from '@/types/icu/chart'
+import { useEffect, useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 import CellsRow from './cells-row'
 import CellsRowTitle from './cells-row-title'
@@ -27,27 +28,33 @@ export default function ChartTable({
 }) {
   const { icu_chart_id, orders } = chartData
   const { setStep } = useIcuOrderStore()
+  const [isSorting, setIsSorting] = useState(true)
+  const [sortedOrders, setSortedOrders] = useState<SelectedIcuOrder[]>([])
 
-  const sortedOrders = useMemo(
-    () =>
-      orders
-        .sort((prev, next) => prev.order_name.localeCompare(next.order_name))
-        .sort(
-          (prev, next) =>
-            DEFAULT_ICU_ORDER_TYPE.map((order) => order.value).findIndex(
-              (order) => order === prev.order_type,
-            ) -
-            DEFAULT_ICU_ORDER_TYPE.map((order) => order.value).findIndex(
-              (order) => order === next.order_type,
-            ),
-        ),
-    [orders],
-  )
+  useEffect(() => {
+    const sorted = [...orders]
+      .sort((prev, next) => prev.order_name.localeCompare(next.order_name))
+      .sort(
+        (prev, next) =>
+          DEFAULT_ICU_ORDER_TYPE.map((order) => order.value).findIndex(
+            (order) => order === prev.order_type,
+          ) -
+          DEFAULT_ICU_ORDER_TYPE.map((order) => order.value).findIndex(
+            (order) => order === next.order_type,
+          ),
+      )
+    setSortedOrders(sorted)
+    setIsSorting(false)
+  }, [orders])
 
   const debouncedSetOrdererSelectStep = useDebouncedCallback(
     () => setStep('selectOrderer'),
-    2000,
+    1000,
   )
+
+  if (isSorting) {
+    return <LargeLoaderCircle className="h-icu-chart" />
+  }
 
   return (
     <Table className="border">
