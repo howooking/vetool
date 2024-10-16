5a791659fcfab1451e834eb359375e8d26f7a1af
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-10-16 11:45:06 +0900

    Update loading.tsx

--- app/hospital/[hos_id]/icu/[target_date]/loading.tsx
+++ app/hospital/[hos_id]/icu/[target_date]/loading.tsx
@@ -2,5 +2,6 @@ import LargeLoaderCircle from '@/components/common/large-loader-circle'
 import React from 'react'
 
 export default function IcuPageLoading() {
+  //test
   return <LargeLoaderCircle className="h-icu-chart" />
 }
