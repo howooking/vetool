67fd4507474185aadce1b2eaac24c8f5b89082c8
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-10-26 23:48:27 +0900

    feat: WIP 입원환자 차트 설명

---
+++ components/hospital/icu/header/instruction/instruction-sidebar.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@/components/ui/button'
+import { INSTRUCTIONS } from '@/constants/hospital/icu/chart/instruction'
+import { cn } from '@/lib/utils'
+import { Info } from 'lucide-react'
+import { Dispatch, SetStateAction } from 'react'
+
+export default function InstructionSidebar({
+  setCurrentVideo,
+  currentVideo,
+}: {
+  setCurrentVideo: Dispatch<
+    SetStateAction<{
+      menuId: number
+      slideId: number
+    }>
+  >
+  currentVideo: {
+    menuId: number
+    slideId: number
+  }
+}) {
+  return (
+    <aside className="w-[200px] border-r">
+      <div className="flex h-12 items-center justify-center gap-1 font-bold shadow-sm">
+        <Info size={18} />
+        <span>입원차트</span>
+      </div>
+      <ul>
+        {INSTRUCTIONS.map((item) => (
+          <li key={item.title}>
+            <Button
+              onClick={() => setCurrentVideo({ menuId: item.id, slideId: 1 })}
+              variant="ghost"
+              className={cn(
+                currentVideo.menuId === item.id && 'bg-muted',
+                'h-10 w-full rounded-none',
+              )}
+            >
+              {item.title}
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
---
+++ constants/hospital/icu/chart/instruction.ts
@@ -0,0 +1,103 @@
+export const INSTRUCTIONS = [
+  {
+    id: 1,
+    title: '환자 입원',
+    slides: [
+      {
+        id: 1,
+        video: '/instruction/register1.mp4',
+        description: '1. 좌측 상단의 환자입원 버튼 클릭',
+      },
+      {
+        id: 2,
+        video: '/instruction/register2.mp4',
+        description: '2. 환자 검색 후 선택하여 입원 등록을 진행',
+      },
+      {
+        id: 3,
+        video: '/instruction/register3.mp4',
+        description: '3. 환자가 없을 경우 신규 환자 등록을 진행',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '차트 생성',
+    slides: [
+      {
+        id: 1,
+        video: '/instruction/chart1.mp4',
+        description: '1-1. 기본차트 생성',
+      },
+      {
+        id: 2,
+        video: '/instruction/chart2.mp4',
+        description: '1-2. 차트 붙여넣기',
+      },
+      {
+        id: 3,
+        video: '/instruction/chart3.mp4',
+        description: '1-3. 템플릿 붙여넣기',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: '오더 등록',
+    slides: [
+      {
+        id: 1,
+        video: '/instruction/order1.mp4',
+        description: '1. 단일 오더 추가 (단축키 ctrl + o)',
+      },
+      {
+        id: 2,
+        video: '/instruction/order2.mp4',
+        description: '2. 오더 시간 추가, 변경 (ctrl 누른상태 + 우클릭)',
+      },
+      {
+        id: 3,
+        video: '/instruction/order3.mp4',
+        description: '3. 오더 순서 변경 (단축키 ctrl + s)',
+      },
+      {
+        id: 4,
+        video: '/instruction/order4.mp4',
+        description:
+          '4. 오더 삭제 - 개별 삭제, 다수 오더 선택(ctrl + 오더클릭) → delete키',
+      },
+      {
+        id: 5,
+        video: '/instruction/order5.mp4',
+        description: '5. 템플릿 오더 추가',
+      },
+      {
+        id: 6,
+        video: '/instruction/order6.mp4',
+        description:
+          '4. 오더 복사 : 다수 오더 선택(ctrl + 오더클릭) → ctrl + c → ctrl + v (타 차트에 붙여넣기 가능)',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '처치 입력',
+    slides: [
+      {
+        id: 1,
+        video: '/instruction/register1.mp4',
+        description: '1. 간단 처치 입력',
+      },
+      {
+        id: 2,
+        video: '/instruction/order2.mp4',
+        description: '2. 상세 처치 입력',
+      },
+      {
+        id: 3,
+        video: '/instruction/register3.mp4',
+        description: '3. 다중 처치 입력',
+      },
+    ],
+  },
+] as const
