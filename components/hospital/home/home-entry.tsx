85b36bf79418b84b505438109cd009bb337fc642
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-11-06 11:11:19 +0900

    refactor: 수퍼계정 홈화면에서만 병원선ㅌ개

--- components/hospital/home/home-entry.tsx
+++ components/hospital/home/home-entry.tsx
@@ -3,22 +3,29 @@
 import Notice from '@/components/hospital/home/notice/notice'
 import Todo from '@/components/hospital/home/todo/todo'
 import { useHospitalHomeRealtime } from '@/hooks/use-hospital-home-realtime'
+import { HosListData } from '@/types/hospital'
 import type { NoticeWithUser } from '@/types/hospital/notice'
 import type { QueriedTodo } from '@/types/hospital/todo'
+import HospitalSelector from '../header/hospital-selector'
 
 export default function HomeEntry({
   noticesData,
   todosData,
   hosId,
+  hosList,
+  isSuper,
 }: {
   noticesData: NoticeWithUser[]
   todosData: QueriedTodo[]
   hosId: string
+  hosList: HosListData[]
+  isSuper: boolean
 }) {
   useHospitalHomeRealtime(hosId)
 
   return (
     <div className="flex w-full flex-col gap-2 p-2 md:flex-row">
+      {isSuper && <HospitalSelector hosList={hosList} />}
       <Notice noticesData={noticesData} hosId={hosId} />
       <Todo todosData={todosData} hosId={hosId} />
     </div>
