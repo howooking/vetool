767f166cdaf606014d39d10351f41f926813c5fd
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2024-11-13 16:17:35 +0900

    refactor: 메모 부분 수정

--- components/hospital/icu/main/chart/selected-chart/chart-body/chart-memos/single-memo/memo-time-stamp.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/chart-memos/single-memo/memo-time-stamp.tsx
@@ -1,27 +1,20 @@
 import { formatTimeDifference } from '@/lib/utils/utils'
 import { format } from 'date-fns'
 import { ChangeEvent } from 'react'
-import type { Memo } from '../chart-memos'
 
 export default function MemoTimeStamp({
-  memo,
   isEditMode,
-  handleEditMemo,
-  memoIndex,
+  setEditedCreateTimestamp,
+  editTimestamp,
+  editedCreateTimestamp,
 }: {
-  memo: Memo
   isEditMode: boolean
-  handleEditMemo: (editedMemo: Memo, memoIndex: number) => Promise<void>
-  memoIndex: number
+  setEditedCreateTimestamp: React.Dispatch<React.SetStateAction<string>>
+  editTimestamp: string | null
+  editedCreateTimestamp: string
 }) {
   const handleChageTimeStamp = (e: ChangeEvent<HTMLInputElement>) => {
-    handleEditMemo(
-      {
-        ...memo,
-        create_timestamp: e.target.value,
-      },
-      memoIndex,
-    )
+    setEditedCreateTimestamp(e.target.value)
   }
 
   return (
@@ -30,17 +23,17 @@ export default function MemoTimeStamp({
         <input
           type="datetime-local"
           className="bg-transparent"
-          value={format(new Date(memo.create_timestamp), "yyyy-MM-dd'T'HH:mm")}
+          value={format(new Date(editedCreateTimestamp!), "yyyy-MM-dd'T'HH:mm")}
           onChange={handleChageTimeStamp}
         />
       ) : (
         <div className="flex items-center gap-2">
           <span>
-            {format(new Date(memo.create_timestamp), 'yyyy MM. dd. HH:mm')}
+            {format(new Date(editedCreateTimestamp), 'yyyy MM. dd. HH:mm')}
           </span>
-          {memo.edit_timestamp && (
+          {editTimestamp && (
             <span suppressHydrationWarning>
-              ({formatTimeDifference(memo.edit_timestamp)} 수정됨)
+              ({formatTimeDifference(editTimestamp)} 수정됨)
             </span>
           )}
         </div>
