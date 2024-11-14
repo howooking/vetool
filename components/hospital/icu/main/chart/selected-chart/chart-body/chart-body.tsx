034d24f88f7de4e78ce2066d879098784f2498fa
Kled <1017yu@daum.net>
2024-11-14 14:41:40 +0900

    feat: 모바일 chart-table 추가

--- components/hospital/icu/main/chart/selected-chart/chart-body/chart-body.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/chart-body.tsx
@@ -3,17 +3,23 @@ import ChartMemos, {
   type Memo,
 } from '@/components/hospital/icu/main/chart/selected-chart/chart-body/chart-memos/chart-memos'
 import ChartTable from '@/components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table'
+import MobileChartTable from '@/components/hospital/icu/main/chart/selected-chart/chart-body/table/mobile/mobile-chart-table'
+import useIsMobile from '@/hooks/use-is-mobile'
 import type { SelectedChart } from '@/types/icu/chart'
 
 export default function ChartBody({ chartData }: { chartData: SelectedChart }) {
   const { memo_a, memo_b, memo_c, icu_chart_id } = chartData
+  const isMobile = useIsMobile()
 
   return (
     <div className="flex flex-col gap-2">
       <ChartInfos chartData={chartData} />
 
-      <ChartTable chartData={chartData} />
-
+      {isMobile ? (
+        <MobileChartTable chartData={chartData} />
+      ) : (
+        <ChartTable chartData={chartData} />
+      )}
       <ChartMemos
         memoA={memo_a as Memo[] | null}
         memoB={memo_b as Memo[] | null}
